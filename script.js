8fffe1f2dced7b65c30ab92d221ddbb883f49bef
RyUmarof <ryumarof@gmail.com>
2025-03-03 22:18:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/** @format */
+
+var typed = new Typed("#Typing", {
+   strings: [
+      "Uy ishi Umarov Abdulloh",
+      "Text Gradients",
+      "Make your headlines stand out",
+   ],
+   typeSpeed: 50,
+   backSpeed: 50,
+   startDelay: 2000,
+   loop: true,
+});
